6cbf11ef36beaf28e111d4dca7511fb13191dcea
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-04-08 21:20:38 -0400

    ok nice

--- App.js
+++ App.js
@@ -1,262 +1,33 @@
-// /assets/loop.wav
-import React, { Component, useState, useEffect } from "react";
-import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  Alert,
-  Image,
-  TouchableOpacity,
-  Text,
-} from "react-native";
-import { Audio } from "expo-av";
+import React from 'react';
+import { StyleSheet, SafeAreaView, Dimensions } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-const buttonColor = ["red"];
+const { width, height } = Dimensions.get('window');
 
-const xy = [require("./assets/boop.wav")];
+const heightNew = height;
+const widthNew = (height * 16) / 9;
 
-const App = () => {
-  const [niemosIterator, setNiemosIterator] = useState(68);
-  const [niemos, setNiemos] = useState([
-    { name: "A", fact: "Fact A", factShort: "Fact A ..." },
-    { name: "B", fact: "Fact B", factShort: "Fact B ..." },
-    { name: "C", fact: "Fact C", factShort: "Fact C ..." },
-  ]);
-
-  const onClickLess = () => {
-    niemos.pop();
-    setNiemosIterator(niemosIterator - 1 < 65 ? 65 : niemosIterator - 1);
-  };
-
-  const onClickMore = () => {
-    goForFetch();
-  };
-
-  const goForFetch = () => {
-    fetch("https://catfact.ninja/fact")
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setNiemos([
-          ...niemos,
-          {
-            name: String.fromCharCode(niemosIterator),
-            fact: responseJson.fact,
-            factShort: responseJson.fact.substr(0, 30) + " ...",
-          },
-        ]);
-        setNiemosIterator(niemosIterator + 1);
-      })
-      .catch((error) => console.log(error));
-  };
-
-  const onClickNiemo = (k) => {
-    handlePlaySound();
-    Alert.prompt(niemos[k].name, niemos[k].fact + "\n" + "\n" + "New Title:", [
-      { text: "✖", onPress: () => {} },
-      {
-        text: "✔",
-        onPress: (text) => {
-          if (text) {
-            niemos[k].name = text;
-            setNiemos([...niemos]);
-          }
-        },
-      },
-    ]);
-  };
-
-  handlePlaySound = async () => {
-    const soundObj = new Audio.Sound();
-
-    console.log("--------------------XXX");
-
-    try {
-      let source = "loop.wav";
-      // let source = "./assets/loop.wav";
-      await soundObj.loadAsync(source);
-      await soundObj
-        .playAsync()
-        .then(async (playbackStatus) => {
-          setTimeout(() => {
-            soundObj.unloadAsync();
-          }),
-            1000;
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={[styles.title, styles.textBig]}>Niemo's Cat Facts</Text>
-      <View style={styles.niemoContainer}>
-        {niemos.map((n, k) => {
-          return (
-            <TouchableOpacity
-              key={k}
-              onPress={() => {
-                onClickNiemo(k);
-              }}
-            >
-              <NiemoComponent
-                key={k}
-                myText={n.name}
-                myFact={n.fact}
-                myFactShort={n.factShort}
-              ></NiemoComponent>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-      <Separator styel={styles.separator} />
-      <View>
-        <View style={styles.buttons}>
-          <TouchableOpacity
-            style={[styles.button, styles.buttonLeft]}
-            onPress={onClickLess}
-          >
-            <Text style={[styles.text, styles.textMedium, styles.textWhite]}>
-              ▼
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.button, styles.buttonRight]}
-            onPress={onClickMore}
-          >
-            <Text style={[styles.text, styles.textMedium, styles.textWhite]}>
-              ▲
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <WebView
+        source={{ uri: 'https://smashed.niembro64.com' }}
+        style={styles.webview}
+      />
     </SafeAreaView>
   );
-};
-
-const Separator = () => <View style={styles.separator} />;
-
-const NiemoComponent = (props) => (
-  <SafeAreaView style={styles.niemo}>
-    <Image style={styles.image} source={require("./assets/mexico.jpg")} />
-    <Text style={[styles.subTitle, styles.textMedium, styles.textWhite]}>
-      {props.myText}
-    </Text>
-    <Text
-      style={[
-        styles.subTitle,
-        styles.textSmall,
-        styles.textWhite,
-        styles.niemoText,
-      ]}
-    >
-      {props.myFactShort}
-    </Text>
-  </SafeAreaView>
-);
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: "100%",
-    backgroundColor: "#fff",
-    // backgroundColor: "#123",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    textAlign: "center",
-    marginVertical: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    // backgroundColor: '#F5FCFF',
   },
-  title: {
-    textAlign: "center",
-    marginVertical: 5,
-  },
-  subTitle: {
-    textAlign: "center",
-    marginVertical: 5,
-  },
-  text: {
-    textAlign: "center",
-    marginVertical: 5,
-    color: "#000000",
-  },
-  textBig: {
-    fontSize: 35,
-  },
-  textMedium: {
-    fontSize: 25,
-  },
-  textSmall: {
-    fontSize: 15,
-  },
-  textWhite: {
-    color: "#fff",
-  },
-  buttons: {
-    width: 350,
-    padding: 5,
-    borderWidth: 0,
-    borderColor: "#00000033",
-    borderRadius: 40,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  button: {
-    fontWeight: "bold",
-    borderRadius: 40,
-    paddingHorizontal: 60,
-    paddingVertical: 10,
-  },
-  separator: {
-    borderBottomColor: "#737373",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    marginVertical: 5,
-  },
-  buttonLeft: {
-    backgroundColor: "#555599",
-  },
-  buttonRight: {
-    backgroundColor: "#995555",
-  },
-  image: {
-    resizeMode: "contain",
-    width: 70,
-    height: 70,
-    borderRadius: 40,
-    margin: 10,
-  },
-  niemoContainer: {
-    borderWidth: 0,
-    borderRadius: 10,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-  },
-  niemo: {
-    backgroundColor: "#114433",
-    maxWidth: 150,
-    margin: 5,
-    borderRadius: 20,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    marginVertical: 5,
-  },
-  niemoText: {
-    maxWidth: 100,
-    margin: 5,
-    borderRadius: 20,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
+  webview: {
+    width: widthNew,
+    height: heightNew,
   },
 });
-
-export default App;
